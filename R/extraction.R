1100615645b5a5390f561f5aad86b7172514a811
Bradley Saul <bradley@BigScreen.local>
2015-12-30 15:37:34 -0500

    outputs list of necessary pieces from a geeglm object

--- R/extraction.R
+++ R/extraction.R
@@ -3,10 +3,21 @@
 #' @param geeobj
 #'
 #' @export
-#
 
 extract_geeglm <- function(geeobj){
-  if(typeof(geeobj) != 'geeglm'){
-    stop("Object must be of type geeglm")
+  if(!('geeglm' %in% class(geeobj) ) ){
+    stop("Object must be of class geeglm")
   }
+
+  out <- list(family = geeobj$family$family,
+              link   = geeobj$family$link,
+              beta   = coef(geeobj),
+              ncoef  = length(coef(geeobj)),
+              nclust = length(unique(geeobj$id)),
+              id     = geeobj$id,
+              data   = cbind(data.frame(response = geeobj$y,
+                                        fitted   = fitted(geeobj),
+                                        weights  = geeobj$weights),
+                             model.matrix(geeobj) ) )
+ return(out)
 }
